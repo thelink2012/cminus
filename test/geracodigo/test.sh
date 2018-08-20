d14f747775e2a8d4c5746cb990bc31e8d32113c0
Denilson das Mercês Amorim <dma_2012@hotmail.com>
2018-08-20 18:22:13 -0300

    Fix tests on Trusty container

--- test/geracodigo/test.sh
+++ test/geracodigo/test.sh
@@ -9,7 +9,7 @@ for infile in *.in; do
     stdout_file="${infile%.*}.stdout"
 
     printf "Testing $infile... "
-    if $GERACODIGO "$infile" "$tempout" && spim -f "$tempout" < "$stdin_file" | tail -n +2 | diff - "$stdout_file" >$tempfile; then
+    if $GERACODIGO "$infile" "$tempout" && spim -f "$tempout" < "$stdin_file" | sed -e '0,/^Loaded:/d' | diff - "$stdout_file" >$tempfile; then
         printf "\033[0;32mOK\033[0m\n"
     else
         printf "\033[0;31mFAILED\033[0m\n"
